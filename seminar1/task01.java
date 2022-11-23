df9534b30cac5e3c9728c0b78adcf65cbe5f4b74
delphython <delphython@gmail.com>
2022-11-23 07:51:04 +0300

    move task01 to folder seminar1

---
+++ seminar1/task01.java
@@ -0,0 +1,16 @@
+/*
+Написать программу, которая запросит пользователя ввести <Имя> в консоли. 
+Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
+*/
+
+import java.io.Console;
+
+public class task01 {
+    public static void main(String[] args) {
+        Console console = System.console();
+        
+        String userName = console.readLine("Enter your name: ");
+
+        console.printf(String.format("Hello, %s!", userName));
+    }
+}
